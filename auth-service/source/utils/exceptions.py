d30745a4218aa3ae1f95eb59c924e6210cce08b3
azya0 <azyadr@yandex.ru>
2026-02-06 18:40:19 +0200

    ref(auth-service): move business logic to /kernel

--- auth-service/source/utils/exceptions.py
+++ auth-service/source/utils/exceptions.py
@@ -3,7 +3,7 @@ from typing import Callable, ParamSpec, TypeVar
 
 from fastapi import HTTPException
 
-from repository.exceptions import RepositoryError
+from service.exceptions import ServiceError
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -14,7 +14,7 @@ def repository_exception_handler(old_function: Callable[P, R]) -> Callable[P, R]
     async def new_function(*args: P.args, **kwargs: P.kwargs) -> R:
         try:
             result = await old_function(*args, **kwargs)
-        except RepositoryError as error:
+        except ServiceError as error:
             raise HTTPException(
                 status_code=error.status,
                 detail=error.message
