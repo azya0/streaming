183389b3ec45e96ab0945df57d16ef22c7fba0ca
azya0 <azyadr@yandex.ru>
2026-02-15 02:36:18 +0200

    feat(auth-service): update User database model
    Add created_at and updated_at fields

--- auth-service/source/database/models.py
+++ auth-service/source/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import String, Integer, Boolean
+from datetime import datetime
+
+from sqlalchemy import String, Integer, Boolean, DateTime, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -14,3 +16,12 @@ class User(Base):
     
     is_active:      Mapped[bool] = mapped_column(Boolean(create_constraint=True), default=True)
     is_admin:       Mapped[bool] = mapped_column(Boolean(), default=False)
+
+    created_at:     Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+
+    # updated field to check refresh/access tokens validation
+    updated_at:     Mapped[datetime] = mapped_column(DateTime,
+        server_default=func.now(),
+        server_onupdate=func.now(),
+        onupdate=datetime.now
+    )
